98e6fd884f326f7f58949376c3fdac37d791f6e7
Lane Howard <405679@colum.edu>
2022-09-19 00:01:22 -0500

    Add project files.

---
+++ LaneAlgorithms/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LaneAlgorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Week1 week1 = new Week1();
+
+            week1.SayFirstItem(week1.numbers);
+            week1.SumOfAllItems(week1.numbers);
+            week1.SumOfAllDifferences(week1.numbers);
+        }
+    }
+}
---
+++ LaneAlgorithms/Week1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaneAlgorithms
+{
+    public class Week1
+    {
+        public List<int> numbers;
+
+        public Week1()
+        {
+            numbers = new List<int>();
+
+            Random r = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                numbers.Add(r.Next(0, 100));
+            }
+
+        }
+
+        /// <summary>
+        /// O(1) or constant 
+        /// because it does the same amount of actions no matter the n
+        /// </summary>
+        /// <param list="n"></param>
+        public void SayFirstItem(List<int> n)
+        {
+            Console.WriteLine("The first item is " + n[0]);
+        }
+        
+        /// <summary>
+        /// O(n) or linear
+        /// because it has to do an action for every n
+        /// </summary>
+        /// <param name="n"></param>
+        public void SumOfAllItems(List<int> n)
+        {
+            int sum = 0;
+            foreach (int i in n)
+            {
+                sum += i;
+            }
+            Console.WriteLine("The sum of all the items is " + sum);
+        }
+        /// <summary>
+        /// O(n^2) or quadratic
+        /// because it loops through n times, and n times for each n. so n * n or n^2.
+        /// </summary>
+        /// <param name="n"></param>
+        public void SumOfAllDifferences(List<int> n)
+        {
+            int sum = 0;
+            foreach (int i in n)
+            {
+                // for each number
+                foreach (int j in n)
+                {
+                    // find the difference with every number
+                    // and add it to the sum
+                    sum += i - j;
+                }
+            }
+            Console.WriteLine("The sum of all the differences is " + sum);
+        }
+
+    }
+}
